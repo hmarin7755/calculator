c4b1c9ed7cbaaed3fb0467de99cc449162505547
Humberto Marin <hmarin2698@gmail.com>
2022-10-14 23:16:54 -0400

    add operations

--- script.js
+++ script.js
@@ -1,22 +1,88 @@
-function add(num1, num2) {
-    return num1 + num2
+let operator = "";
+let prevValue = "";
+let currentVal = "";
+let prevDisplay = document.querySelector(".prev-display-text");
+let currentDisplay = document.querySelector(".curr-display-text");
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".op-button");
+let clear = document.querySelector(".clear");
+let equal = document.querySelector(".equals-button");
+let decimal = document.querySelector(".dot");
+let negate = document.querySelector(".negate");
+
+equal.addEventListener("click", () => {
+  calculate();
+  prevDisplay.textContent = "";
+  currentDisplay.textContent = prevValue;
+});
+
+decimal.addEventListener('click', ()=>{
+    addDecimal();
+})
+
+function addDecimal(){
+    if(!currentVal.includes('.')){
+        currentVal += '.';
+        currentDisplay.textContent += '.'
+    }
 }
 
-function subtract(num1, num2) {
-    return num1-num2;
+function calculate() {
+  prevValue = Number(prevValue);
+  currentVal = Number(currentVal);
+
+  if (operator === "+") {
+    prevValue += currentVal;
+  } else if (operator === "-") {
+    prevValue -= currentVal;
+  } else if (operator === "÷") {
+    prevValue /= currentVal;
+  } else {
+    prevValue *= currentVal;
+  }
+
+  prevValue = roundNum(prevValue);
+  prevValue = prevValue.toString();
+  prevValue = prevValue.toString();
 }
 
-function multiply(num1, num2) {
-    return num1 * num2;
+function roundNum(num) {
+  return Math.round(num * 1000) / 1000;
 }
 
-function divide(num1, num2) {
-    return num1 / num2; 
+clear.addEventListener("click", () => {
+  prevValue = "";
+  currentVal = "";
+  operator = "";
+  prevDisplay.textContent = currentVal;
+  currentDisplay.textContent = currentVal;
+});
+
+numbers.forEach((number) =>
+  number.addEventListener("click", (e) => {
+    handleNumber(e.target.textContent);
+    currentDisplay.textContent = currentVal;
+  })
+);
+
+function handleNumber(num) {
+  if (currentVal.length < 6) {
+    currentVal += num;
+  }
 }
 
-function operate(operator, num1, num2) {
-    return operator(num1, num2);
-}
+operators.forEach((op) =>
+  op.addEventListener("click", (e) => {
+    handleOperator(e.target.textContent);
+    prevDisplay.textContent = `${prevValue} ${operator}`;
+    currentDisplay.textContent = currentVal;
+  })
+);
 
+function handleOperator(op) {
+  operator = op;
+  prevValue = currentVal;
+  currentVal = "";
+}
 
 //console.log(operate(add, 5, 2))
