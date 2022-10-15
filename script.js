edda0ba56e9efcde29ddf5e66b98aaa76cdbc8bf
Humberto Marin <hmarin2698@gmail.com>
2022-10-15 00:01:43 -0400

    add clear button

--- script.js
+++ script.js
@@ -8,7 +8,25 @@ let operators = document.querySelectorAll(".op-button");
 let clear = document.querySelector(".clear");
 let equal = document.querySelector(".equals-button");
 let decimal = document.querySelector(".dot");
+//TODO fix functionality for these buttons
 let negate = document.querySelector(".negate");
+let backspace = document.querySelector(".backspace");
+//TODO fix negate after pressing equals, if u press negate iagain it brings up last operand used
+negate.addEventListener("click", () => {
+  negateNum();
+});
+
+function negateNum() {
+
+  currentVal = -Math.abs(Number(currentVal));
+  currentDisplay.textContent = currentVal.toString();
+  
+}
+//TODO backspace doesnt work after negating number
+backspace.addEventListener("click", () => {
+    currentVal = currentVal.slice(0, currentVal.length - 1);
+    currentDisplay.textContent = currentVal;
+});
 
 equal.addEventListener("click", () => {
   calculate();
@@ -16,15 +34,15 @@ equal.addEventListener("click", () => {
   currentDisplay.textContent = prevValue;
 });
 
-decimal.addEventListener('click', ()=>{
-    addDecimal();
-})
+decimal.addEventListener("click", () => {
+  addDecimal();
+});
 
-function addDecimal(){
-    if(!currentVal.includes('.')){
-        currentVal += '.';
-        currentDisplay.textContent += '.'
-    }
+function addDecimal() {
+  if (!currentVal.includes(".")) {
+    currentVal += ".";
+    currentDisplay.textContent += ".";
+  }
 }
 
 function calculate() {
